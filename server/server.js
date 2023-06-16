82ad0741b68c9ea1957db8f63993626966ac8864
Denis Beresnev <shanthi.bunch@gmail.com>
2023-06-16 16:12:04 +0300

    init project
    npm install express express-graphql graphql --save
    npm install --save-dev nodemon
    npm install dotenv --save

---
+++ server/server.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
