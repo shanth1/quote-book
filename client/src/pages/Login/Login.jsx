81a3028f7edae2c0d2aa39efa8836053e293b85f
Denis Beresnev <shanthi.bunch@gmail.com>
2023-06-25 19:27:34 +0500

    tailwind set up

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.scss";
 export const Login = () => {
     return (
         <div className={styles.wrapper}>
-            <div>
+            <div className="">
                 <h1>Sign in</h1>
             </div>
             <div className={styles.form}></div>
