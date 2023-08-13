f9ee50be9a51f3d91cf94b0a906e111079aa2b00
Denis Beresnev <shanthi.bunch@gmail.com>
2023-08-13 14:29:38 +0300

    clear authors when type is person
    changed Input component
    change add and update box mutation

--- client/src/shared/Input/Input.jsx
+++ client/src/shared/Input/Input.jsx
@@ -14,7 +14,7 @@ const Input = ({
             className=" bg-gray-50 h-10 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             type={type}
             name={name}
-            value={value}
+            value={disabled ? "" : value}
             placeholder={placeholder}
             onChange={onChange}
         />
