a8c5f7dd9b8bb9b1838d43ba201aecc91b495193
Denis Beresnev <shanthi.bunch@gmail.com>
2023-08-15 22:57:52 +0300

    go back feature in Box page, remove time

--- client/src/pages/Collections/pages/Box/Box.jsx
+++ client/src/pages/Collections/pages/Box/Box.jsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@apollo/client";
 import { GET_BOX_QUOTES } from "../../../../graphql/queries";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { QuoteList } from "../../../../widgets/QuoteList/QuoteList";
 import Content from "../../../../shared/Content/Content";
 import H1 from "../../../../shared/H1/H1";
@@ -10,6 +10,7 @@ import { NotFound } from "../../../NotFound/NotFound";
 
 const Box = () => {
     const { logout } = useContext(AuthContext);
+    const navigate = useNavigate();
 
     const { boxId } = useParams();
     const { loading, error, data } = useQuery(GET_BOX_QUOTES, {
@@ -29,7 +30,29 @@ const Box = () => {
 
     return (
         <Content>
-            <H1>{data?.getBox?.title || "Box"}</H1>
+            <div className="flex gap-2 items-center">
+                <div
+                    onClick={() => navigate(-1)}
+                    className="flex justify-center items-center w-10 h-10 bg-white rounded-full cursor-pointer shadow-sm"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="w-5 h-5 rtl:rotate-180"
+                    >
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+                        />
+                    </svg>
+                </div>
+                <H1>{data?.getBox?.title || "Box"}</H1>
+            </div>
+
             <QuoteList quotes={quotes} />
         </Content>
     );
--- client/src/utils/dateString.js
+++ client/src/utils/dateString.js
@@ -1,8 +1,8 @@
 export const getStringFromDate = (date) => {
     const localeDate = date.toLocaleDateString();
     return String.prototype.concat(
-        date.toLocaleTimeString().slice(0, 5),
-        " ",
+        // date.toLocaleTimeString().slice(0, 5),
+        // " ",
         localeDate.slice(0, -4),
         localeDate.slice(-2),
     );
