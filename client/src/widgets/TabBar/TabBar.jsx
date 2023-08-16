c3f9a7437d5e852f8247caacc4ac631aad2f7b32
Denis Beresnev <shanthi.bunch@gmail.com>
2023-08-16 13:21:39 +0300

    Refactor TabBar and padding-bottom in Layout

--- client/src/widgets/TabBar/TabBar.jsx
+++ client/src/widgets/TabBar/TabBar.jsx
@@ -16,7 +16,7 @@ export const TabBar = () => {
 
     return (
         <div className="flex justify-center">
-            <div className="fixed w-56 shadow-md transition-all justify-evenly flex items-center bottom-10 gap-4 bg-white rounded-lg p-1">
+            <div className="fixed w-48 shadow-md transition-all justify-evenly flex items-center bottom-8 gap-4 bg-white rounded-lg p-1">
                 <NavLink to="boxes">
                     {({ isActive }) => (
                         <div
